d09bee8b35976c514ba6d73e0f441ad25da9d981
DerEchtePilz <tlfm90@gmail.com>
2023-07-16 01:24:51 +0200

    Update upstream to point to 1.20.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Locale
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
@@ -5,6 +7,32 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+}
+
+if (!file(".git").exists()) {
+    val errorText = """
+        
+        =====================[ ERROR ]=====================
+         The Scissors project directory is not a properly cloned Git repository.
+         
+         In order to build Scissors from source you must clone
+         the Scissors repository using Git, not download a code
+         zip from GitHub.
+         
+         See <coming soon>
+         for further information on building and modifying Paper and Forks
+         just like this.
+        ===================================================
+    """.trimIndent()
+    error(errorText)
+}
+
 rootProject.name = "Scissors"
 
-include("scissors-api", "scissors-server")
+for (name in listOf("scissors-api", "scissors-server")) {
+    val projName: String = name.toLowerCase(Locale.ENGLISH)
+    include(projName)
+    findProject(":$projName")!!.projectDir = file(name)
+}
